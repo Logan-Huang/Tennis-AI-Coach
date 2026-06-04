d8b2f608dd4bdce03d5c57e606d5879f58617410
Logan Huang <loganhtheofficial@gmail.com>
2026-06-04 19:30:33 -0400

    Initial Commit

---
+++ Tennis AI Coach/Tennis_AI_CoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tennis_AI_CoachApp.swift
+//  Tennis AI Coach
+//
+//  Created by Logan Huang on 6/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Tennis_AI_CoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
